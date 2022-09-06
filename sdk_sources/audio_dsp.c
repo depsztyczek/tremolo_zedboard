bed086d5c25778d9318627ea77984b3b750b5646
r4jmond <domino5740@o2.pl>
2022-09-06 19:35:40 +0200

    Added UART configuration in a loop.

--- sdk_sources/audio_dsp.c
+++ sdk_sources/audio_dsp.c
@@ -1,17 +1,9 @@
 #include "audio_dsp.h"
-#include "axi_helper.h"
 #include "xil_printf.h"
 #include "uart_helper.h"
 
 int main(void)
 {
-
-	char8 recChar;
-	u32 enableTremolo;
-	u32 depth;
-	u32 frequency;
-
-
 	xil_printf("Entering Main\r\n");
 	//Configure the IIC data structure
 	IicConfig(XPAR_XIICPS_0_DEVICE_ID);
@@ -25,35 +17,10 @@ int main(void)
 	AudioConfigureJacks();
 	xil_printf("ADAU1761 configured\r\n");
 
-
-	xil_printf("Enable Tremolo effect? (Enter 1 or 0)\r\n");
-	outbyte (recChar = inbyte());
-	enableTremolo = (UART_GetDecDigit(recChar));
-
-	if(enableTremolo == ENABLE_TREMOLO)
+	while(1)
 	{
-		xil_printf("\n\rEnter depth [-1, 1)\r\n");
-		depth = UART_GetFXP_SQ0_23();
-
-		xil_printf("Enter frequency as integer [1-999]\r\n");
-		frequency = UART_ReadThreeDigitDecVal();
-
-		if((depth == 0) | (frequency == 0))
-		{
-			configureTremolo(DISABLE_TREMOLO, 0, 0);
-			xil_printf("\r\nConfiguration erroneous, try again. Tremolo disabled.\r\n.");
-		}
-		else
-		{
-			configureTremolo(enableTremolo, depth, frequency);
-			xil_printf("\r\nTremolo configured. Depth: %x, Freq: %d.\r\n", depth, frequency);
-		}
-
+		UART_TremoloConfiguration();
 	}
-	else
-	{
-		configureTremolo(DISABLE_TREMOLO, 0, 0);
-		xil_printf("\r\nTremolo disabled, entering loopback mode.\r\n");
-	}
+
 }
 
